74b36a765917410499aaf1dd05599575834b622a
Doum <ehdna36@gmail.com>
2017-06-13 15:09:10 +0900

    css 반영 - 게시판 깨짐

--- src/main/webapp/assets/js/common.js
+++ src/main/webapp/assets/js/common.js
@@ -1,14 +1,15 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $("#header .menu").click(function () {
-        $("#sub-menu").addClass("active");
-        $("body").css("overflow", "hidden");
-    });
+$("#header .menu").click(function() {
+  $("#sub-menu").addClass("active");
+  $("body").css("overflow", "hidden");
+});
 
-    $("#sub-menu .close").click(function () {
-        $("#sub-menu").removeClass("active");
-        $("body").removeAttr("style");
-    });
+$("#sub-menu .close").click(function() {
+  $("#sub-menu").removeClass("active");
+  $("body").removeAttr("style");
+});
+
 
 
 });
