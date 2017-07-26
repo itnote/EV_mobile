6a857538dd4224859f85275ae7542707a75aa26d
Doum <ehdna36@gmail.com>
2017-07-26 18:33:51 +0900

    인증마크 css 추가

---
+++ src/main/webapp/js/common.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+$("#header .menu").click(function() {
+  $("#sub-menu").addClass("active");
+  $("body").css("overflow", "hidden");
+});
+
+$("#sub-menu .close").click(function() {
+  $("#sub-menu").removeClass("active");
+  $("body").removeAttr("style");
+});
+
+$(".pop-btn").click(function() {
+  var popon = $(this).attr("data");
+
+  $("div#"+ popon).show();
+
+});
+
+
+});
